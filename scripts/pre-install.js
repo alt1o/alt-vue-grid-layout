9447f19bffc94b282420d3521f13bee124b0535b
wxl <wangxinglong@fxiaoke.com>
2019-02-23 15:23:11 +0800

    test: dev

--- scripts/pre-install.js
+++
@@ -1,9 +0,0 @@
-let fs = require('fs');
-
-let pkg = require('../package.json');
-pkg.dependencies = {};
-pkg.devDependencies = {};
-
-fs.writeFile('../package.json', JSON.stringify(pkg), (err) => {
-    if(err) throw err;
-})
